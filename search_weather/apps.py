5cb84d730bab0eddbb35e27dfd48e22b4b3d99a0
anilkumar512 <anilkumar9120255@gmail.com>
2025-03-29 22:22:27 +0530

    first commit

---
+++ search_weather/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SearchWeatherConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'search_weather'
---
+++ search_weather/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+import requests
+from django.shortcuts import redirect, render
+from .models import Weather
+# Create your views here.
+def search(request):
+    city=request.GET.get('q','')
+    context={'city':city}
+    if city :
+        URL=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=3558832005761a9148ad56b0533702b7'
+        resp=requests.get(url=URL)
+        if resp.status_code==200:
+            data=resp.json()
+            context['data']=data
+            context['temperature']=round(data['main']['temp']-273.15,2)
+        else:
+            context['data']=None
+            context['error']='City Not Found or API error'
+
+
+    return render(request,'search.html',context)
